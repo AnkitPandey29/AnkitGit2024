6a9263ee54396632827a9391e8a127b7d0b98c09
AnkitPandey29 <113445269+AnkitPandey29@users.noreply.github.com>
2024-04-06 15:04:19 +0530

    Update IfElseExample.java

--- Automation2024/src/test/java/javaBasicProgram/IfElseExample.java
+++ Automation2024/src/test/java/javaBasicProgram/IfElseExample.java
@@ -6,7 +6,7 @@ public class IfElseExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		//capagamin interview
 		Scanner s = new Scanner(System.in);
 		System.out.println("Input the Number");
 		int number = s.nextInt();
